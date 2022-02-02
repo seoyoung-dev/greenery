fde609eab2950bed70885cd16f547e46c215543a
ahn0min <ahn0min95@gmail.com>
2022-02-03 01:09:05 +0900

    feat: GlobalStyle 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import GlobalStyle from "./style/GlobalStyle";
+
 import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
+      <GlobalStyle />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
