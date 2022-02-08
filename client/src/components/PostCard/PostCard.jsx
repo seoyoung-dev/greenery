768bc4e2c744d2044fd24c7c426f1897f801ac94
김동현 <kdhahtks@gmail.com>
2022-02-08 11:11:33 +0000

    Front/community

---
+++ client/src/components/PostCard/PostCard.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faThumbsUp } from "@fortawesome/free-solid-svg-icons";
+import { PostCardArticle, ImgWrapper, ContentBox, Profile, Like } from './PostCard.style'
+
+const dummy = {
+  postImgUrl: "img/post.png",
+  title: "함께 한지 2년된 호호노롤종",
+  profileImgUrl: "img/profile.png",
+  author: "elice",
+  likeNum: 4876,
+};
+
+export function PostCard({ postImgUrl, title, profileImgUrl, author, likeNum }) {
+  return (
+    <PostCardArticle>
+      <ImgWrapper>
+        <img src={ postImgUrl || dummy.postImgUrl } alt="포스트 이미지" />
+      </ImgWrapper>
+      <h3>{ title || dummy.title }</h3>
+      <ContentBox>
+        <Profile>
+          <img src={ profileImgUrl || dummy.profileImgUrl } alt="프로필 이미지" />
+          <span>{ author || dummy.author }</span>
+        </Profile>
+        <Like>
+          <FontAwesomeIcon icon={faThumbsUp} />
+          <span>{ likeNum || dummy.likeNum }</span>
+        </Like>
+      </ContentBox>
+    </PostCardArticle>
+  );
+}
