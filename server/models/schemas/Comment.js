05187eff55bcb1e4993c6d371487c526288af43a
ahn0min <ahn0min95@gmail.com>
2022-02-09 03:42:33 +0900

    add: server directory 최신화"

---
+++ server/models/schemas/Comment.js
@@ -0,0 +1,25 @@
+const Schema = require("mongoose");
+
+const commentSchema = new Schema.Schema(
+  {
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    content: {
+      type: String,
+      require: true,
+    },
+    level: {
+      type: Number,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = commentSchema;
