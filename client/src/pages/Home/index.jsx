ef659397a5794e04c364adae2d04ffee494be2ba
ahn0min <ahn0min95@gmail.com>
2022-02-09 01:57:33 +0900

    chore(package): client proxy 설정

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,7 +1,20 @@
+import axios from "axios";
 import Container from "containers/Home/testfile";
 
 function Home() {
-  return <Container />;
+  return (
+    <>
+      <Container />
+      <button
+        onClick={async () => {
+          const response = await axios.get("/user");
+          console.log(response);
+        }}
+      >
+        Proxy
+      </button>
+    </>
+  );
 }
 
 export default Home;
