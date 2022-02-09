223e3e09d8e1c769775612d1e398420e81049d07
seoyoung-dev <seoyoung.dev@gmail.com>
2022-02-09 14:58:37 +0900

    postbox 안에 postbox 불러오기

--- client/src/containers/Home/Home.jsx
+++ client/src/containers/Home/Home.jsx
@@ -1,12 +1,58 @@
 import React from "react";
-import { Main } from "./Home.style";
-import PostCardBox from "../../components/PostCardBox";
+import { Main, PostContentsWrapper, Title, PostCardBox } from "./Home.style";
+import TodaysPlant from "../../components/TodaysPlant";
+import PostCard from "../../components/PostCard";
 import ImgCard from "../../components/ImgCard";
+
+const dummy = [
+  {
+    postId: 1,
+    postImgUrl: "img/post.png",
+    title: "안녕 나는 첫번째 데이터야",
+    profileImgUrl: "img/profile.png",
+    author: "elice-one",
+    likeNum: 11111,
+  },
+  {
+    postId: 2,
+    postImgUrl: "img/post.png",
+    title: "안녕 나는 두번째 데이터야",
+    profileImgUrl: "img/profile.png",
+    author: "elice-two",
+    likeNum: 22222,
+  },
+  {
+    postId: 3,
+    postImgUrl: "img/post.png",
+    title: "안녕 나는 세번째 데이터야",
+    profileImgUrl: "img/profile.png",
+    author: "elice-three",
+    likeNum: 33333,
+  },
+];
 
 export function Home() {
   return (
     <Main>
-      <PostCardBox />
+      <TodaysPlant />
+      <PostContentsWrapper>
+        <Title>
+          <span>많이 본 초록이들</span>
+          <span>더 많은 초록이들 보기</span>
+        </Title>
+        <PostCardBox>
+          {dummy.map(card => (
+            <PostCard
+              key={card.postId}
+              postImgUrl={card.postImgUrl}
+              title={card.title}
+              profileImgUrl={card.profileImgUrl}
+              author={card.author}
+              likeNum={card.likeNum}
+            />
+          ))}
+        </PostCardBox>
+      </PostContentsWrapper>
       <ImgCard />
     </Main>
   );
--- client/src/containers/Home/Home.style.js
+++ client/src/containers/Home/Home.style.js
@@ -6,3 +6,31 @@ export const Main = styled.main`
   justify-content: space-between;
   align-items: center;
 `;
+
+export const PostContentsWrapper = styled.div`
+  width: 1136px;
+  margin: 80px 0 100px 0;
+`;
+
+export const Title = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-bottom: 24px;
+
+  & span {
+    font-size: 20px;
+    font-weight: bold;
+  }
+
+  & :nth-child(2) {
+    font-size: 14px;
+    color: #767676;
+  }
+`;
+
+export const PostCardBox = styled.div`
+  display: flex;
+  width: 1136px;
+  justify-content: space-between;
+`;
