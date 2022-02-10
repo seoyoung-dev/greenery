e9fe52c34e6e7f96d1ef079b0486aca71deecd71
조서영 <jojo.fedev@gmail.com>
2022-02-10 17:22:14 +0900

    sidbar img tag attribute 수정

--- client/src/components/SideBar/SideBar.jsx
+++ client/src/components/SideBar/SideBar.jsx
@@ -5,16 +5,16 @@ export default function SideBar() {
   return (
     <IconBox>
       <Icon>
-        <img src={"icon/up.svg"}></img>
+        <img src={"icon/thumbs-up.svg"} alt="thumbs-up"></img>
       </Icon>
       <Icon>
-        <img src={"icon/comment.svg"}></img>
+        <img src={"icon/comment.svg"} alt="comment"></img>
       </Icon>
       <Icon>
-        <img src={"icon/edit.svg"}></img>
+        <img src={"icon/edit.svg"} alt="edit"></img>
       </Icon>
       <Icon>
-        <img src={"icon/trash.svg"}></img>
+        <img src={"icon/trash.svg"} alt="delete"></img>
       </Icon>
     </IconBox>
   );
