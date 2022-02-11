2ad805e3aa2597b91f29ae23282a77ee47f7bcbd
정병욱 <korjby400@gmail.com>
2022-02-12 03:12:44 +0900

    Post Page 백엔드 연결

--- client/src/components/PostForm/PostForm.jsx
+++ client/src/components/PostForm/PostForm.jsx
@@ -1,14 +1,129 @@
-import React from "react";
-import { PostTextarea, UploadImg, ImgForm } from "./PostForm.style";
+import axios from "axios";
+import React, { useState, useRef } from "react";
+import {
+  PostTextarea,
+  UploadImg,
+  CotentAddButton,
+  PostTitle,
+  RemoveBtn,
+  ContentSection,
+  PostFormWrapper,
+} from "./PostForm.style";
 
 export default function PostForm() {
+  const [title, setTitle] = useState("");
+  const inputRef = useRef([]);
+  const [inputList, setInputList] = useState([
+    {
+      text: "",
+      imgUrl: "",
+      // fileImage: "/images/userProfile/default.png",
+    },
+  ]);
+
+  // handle input change
+  const handleInputChange = (e, index) => {
+    const { name, value } = e.target;
+    const list = [...inputList];
+
+    if (name === "fileImage") {
+      list[index][name] = URL.createObjectURL(e.target.files[0]);
+      list[index].imgUrl = "";
+    } else list[index][name] = value;
+    setInputList(list);
+  };
+
+  // handle click event of the Remove button
+  const handleRemoveClick = index => {
+    const list = [...inputList];
+    list.splice(index, 1);
+    setInputList(list);
+  };
+
+  // handle click event of the Add button
+  const handleAddClick = () => {
+    setInputList([...inputList, { text: "", imgUrl: "" }]);
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    const data = new FormData();
+    // console.log(inputRef);
+
+    data.append("title", title);
+    inputList.forEach((content, i) => {
+      data.append("userfiles", inputRef.current[i].files[0]);
+      data.append(`contents[${i}]`, content.text);
+      data.append(`img[${i}]`, content.imgUrl);
+    });
+
+    axios
+      .post("/posts", data, {
+        headers: { "Content-Type": "multipart/form-data" },
+      })
+      .then(res => console.log(res.data));
+
+    console.log(data.getAll("title"));
+    console.log(data.getAll("userfiles"));
+    console.log(data.getAll("contents"));
+    console.log(data.getAll("img"));
+  };
+
   return (
-    <ImgForm>
-      <label id="file-button" for="upload-file">
-        <UploadImg src="/img/upload.png" alt="upload.png" />
-        <input type="file" id="upload-file" style={{ display: "none" }} />
-      </label>
-      <PostTextarea placeholder="사진에 대해 설명해주세요."></PostTextarea>
-    </ImgForm>
+    <PostFormWrapper>
+      <PostTitle
+        placeholder="제목을 입력해주세요."
+        onChange={e => setTitle(e.target.value)}
+      />
+      <form onSubmit={onSubmit}>
+        {inputList.map((x, i) => {
+          return (
+            <ContentSection key={i}>
+              {/* {console.log(x)} */}
+              <label htmlFor={x.uploadFile}>
+                <input
+                  ref={e => {
+                    inputRef.current[i] = e;
+                  }}
+                  type="file"
+                  id={x.uploadFile}
+                  name="fileImage"
+                  accept="image/*"
+                  onChange={e => handleInputChange(e, i)}
+                  style={{ display: "none" }}
+                />
+
+                {x.fileImage ? (
+                  <UploadImg
+                    style={{ width: 420, height: 350, objectFit: "cover" }}
+                    src={x.fileImage}
+                    alt="upload.png"
+                  />
+                ) : (
+                  <UploadImg src="/img/upload.png" alt="upload.png" />
+                )}
+              </label>
+
+              <PostTextarea
+                name="text"
+                placeholder="내용을 입력하세요."
+                value={x.text}
+                onChange={e => handleInputChange(e, i)}
+              />
+              {inputList.length !== 1 && (
+                <RemoveBtn onClick={() => handleRemoveClick(i)}>
+                  <img src="/img/closeIcon.png" />
+                </RemoveBtn>
+              )}
+            </ContentSection>
+          );
+        })}
+        <button type="submit">보내기</button>
+      </form>
+
+      <CotentAddButton onClick={handleAddClick}>추가하기</CotentAddButton>
+
+      {/* <div>{JSON.stringify(inputList)}</div> */}
+    </PostFormWrapper>
   );
 }
--- client/src/components/PostForm/PostForm.style.js
+++ client/src/components/PostForm/PostForm.style.js
@@ -1,5 +1,40 @@
 import styled from "styled-components";
 
+export const PostFormWrapper = styled.div``;
+
+export const PostTitle = styled.input`
+  width: 862px;
+  height: 44px;
+  margin: 32px 0px 32px 0px;
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 17px;
+
+  line-height: 20px;
+  text-indent: 10px;
+
+  border-radius: 5px;
+  border: 1px solid #e5e5e5;
+
+  &:focus {
+    outline: none;
+    border: 1px solid #45ba66;
+  }
+`;
+
+export const ContentSection = styled.div`
+  position: relative;
+
+  margin-bottom: 32px;
+`;
+
+export const UploadImg = styled.img`
+  border: 1px solid #dadce0;
+  border-radius: 10px;
+`;
+
 export const PostTextarea = styled.textarea`
   width: 420px;
   height: 153px;
@@ -8,28 +43,54 @@ export const PostTextarea = styled.textarea`
   font-style: normal;
   font-weight: 500;
   font-size: 17px;
+
   line-height: 20px;
-
   margin-left: 22px;
-
   resize: none;
-
   padding: 10px;
+
   border-radius: 5px;
   border: 1px solid #e5e5e5;
+
   &:focus {
     outline: none;
     border: 1px solid #45ba66;
   }
 `;
 
-export const UploadImg = styled.img`
-  width: 420;
-  height: 350;
-  border: 1px solid #dadce0;
-  border-radius: 10px;
+export const RemoveBtn = styled.button`
+  position: absolute;
+  top: 0;
+  right: 0;
+
+  background-color: transparent;
+  border: none;
+
+  img {
+    width: 30px;
+    height: 26px;
+  }
+  left: 820px;
+  bottom: 300px;
 `;
 
-export const ImgForm = styled.div`
-  margin-bottom: 35px;
+export const CotentAddButton = styled.button`
+  width: 862px;
+  height: 64px;
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 18px;
+
+  line-height: 21px;
+  align-items: center;
+  text-align: center;
+
+  border-radius: 5px;
+  border: 1px solid #e5e5e5;
+
+  background: #f5f5f5;
+
+  color: #767676;
 `;
