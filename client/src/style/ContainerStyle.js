0f417743edfd215142930e733e31f9a55eaa7afc
hej <dark199174@gmail.com>
2022-02-12 19:35:30 +0900

    Fix container style center

--- client/src/style/ContainerStyle.js
+++ client/src/style/ContainerStyle.js
@@ -2,12 +2,15 @@
 
 export const WideContainer = styled.div`
   width: clamp(0px, 100%, 1140px);
+  margin: 0 auto;
 `;
 
 export const MediumContainer = styled.div`
   width: clamp(0px, 100%, 720px);
+  margin: 0 auto;
 `;
 
 export const SmallContainer = styled.div`
   width: clamp(0px, 100%, 360px);
+  margin: 0 auto;
 `;
