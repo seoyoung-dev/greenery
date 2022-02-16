8512f3cbc6deaf851c6f379bc55e4b9745ed9a71
이원형 <lk8969@gmail.com>
2022-02-16 07:30:25 +0000

    오늘의 게시글 컴포넌트 수정

--- client/src/components/SideBar/SideBar.jsx
+++ client/src/components/SideBar/SideBar.jsx
@@ -6,16 +6,16 @@ export default function SideBar() {
     <SideBarWrapper>
       <IconBox>
         <Icon width={20.62}>
-          <img src="icon/thumbs-up.svg" alt="thumbs-up" />
+          <img src="/icon/thumbs-up.svg" alt="thumbs-up" />
         </Icon>
         <Icon width={22}>
-          <img src="icon/comment.svg" alt="comment" />
+          <img src="/icon/comment.svg" alt="comment" />
         </Icon>
         <Icon width={22}>
-          <img src="icon/edit.svg" alt="edit" />
+          <img src="/icon/edit.svg" alt="edit" />
         </Icon>
         <Icon width={19.25}>
-          <img src="icon/trash.svg" alt="delete" />
+          <img src="/icon/trash.svg" alt="delete" />
         </Icon>
       </IconBox>
     </SideBarWrapper>
