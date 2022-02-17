a27e9742a8b29d474ba500556459bc4de97ca1fe
ahn0min <ahn0min95@gmail.com>
2022-02-18 05:49:07 +0900

    fix: 무한스크롤 수정

--- client/src/containers/Community/Community.jsx
+++ client/src/containers/Community/Community.jsx
@@ -8,46 +8,82 @@ import axios from "axios";
 
 export function Community() {
   const [posts, setPosts] = useState([]);
-  const [page, setPageNum] = useState(1);
-  const intersectionRef = useRef(null);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const pageEnd = useRef(null);
 
-  const getPosts = useCallback(async page => {
+  const getPosts = async page => {
     const url = `/api/posts/page?page=${page}`;
     const response = await axios.get(url, {
-      query: { page },
+      qeury: { page },
     });
+
     setPosts(prev => {
       const newPosts = [...prev, ...response.data.posts];
       return newPosts;
     });
-  }, []);
-
-  const options = {
-    root: null, // 관찰대상의 부모요소
-    rootMargin: "300px", // 뷰포트의 마진
-    threshold: 1, // 0 ~ 1 겹치는 정도
+    setLoading(true);
   };
-
-  const handleObserver = useCallback(async entires => {
-    const target = entires[0];
-    if (target.isIntersecting) {
-      setPageNum(prev => prev + 1);
-    }
-    return;
-  }, []);
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(handleObserver, options);
-    if (intersectionRef.current) {
-      observer.observe(intersectionRef.current);
-    }
-    return () => observer.disconnect();
-  }, [handleObserver]);
 
   useEffect(() => {
     getPosts(page);
   }, [page]);
 
+  const increasePage = () => {
+    setPage(prev => prev + 1);
+  };
+
+  const ovserveHandler = useCallback(entires => {
+    if (entires[0].isIntersecting) {
+      increasePage();
+    }
+  });
+  useEffect(() => {
+    if (loading) {
+      const observer = new IntersectionObserver(ovserveHandler, {
+        threshold: 1,
+      });
+      observer.observe(pageEnd.current);
+    }
+  }, [loading]);
+
+  // const getPosts = useCallback(async page => {
+  //   const url = `/api/posts/page?page=${page}`;
+  //   const response = await axios.get(url, {
+  //     query: { page },
+  //   });
+  //   setPosts(prev => {
+  //     const newPosts = [...prev, ...response.data.posts];
+  //     return newPosts;
+  //   });
+  // }, []);
+
+  // const options = {
+  //   root: null, // 관찰대상의 부모요소
+  //   rootMargin: "300px", // 뷰포트의 마진
+  //   threshold: 1, // 0 ~ 1 겹치는 정도
+  // };
+
+  // const handleObserver = useCallback(async entires => {
+  //   const target = entires[0];
+  //   if (target.isIntersecting) {
+  //     setPageNum(prev => prev + 1);
+  //   }
+  //   return;
+  // }, []);
+
+  // useEffect(() => {
+  //   const observer = new IntersectionObserver(handleObserver, options);
+  //   if (intersectionRef.current) {
+  //     observer.observe(intersectionRef.current);
+  //   }
+  //   return () => observer.disconnect();
+  // }, [handleObserver]);
+
+  // useEffect(() => {
+  //   getPosts(page);
+  // }, [page]);
+  console.log(posts, page);
   return (
     <>
       <Header />
@@ -68,7 +104,7 @@ export function Community() {
                   />
                 );
               })}
-            <div ref={intersectionRef} style={{ position: "hidden" }}></div>
+            <div ref={pageEnd} style={{ position: "hidden" }}></div>
           </ContentsWrapper>
           <PostButtonWrapper>
             <Link to="/post">
